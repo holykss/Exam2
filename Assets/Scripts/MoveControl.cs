577b0d936dcedeb79ab99d54d692e586bfba90f1
holykss <holykss@gmail.com>
2012-10-11 17:57:49 +0900

    fighter script

--- Assets/Scripts/MoveControl.cs
+++ Assets/Scripts/MoveControl.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class MoveControl : MonoBehaviour {
-
+	
+	private Transform playerTr;
+	public float speed = 10.0f;
+	
 	// Use this for initialization
 	void Start () {
-	
+		playerTr = gameObject.GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		playerTr.Translate(Vector3.forward * Time.deltaTime * speed);
 	
 	}
 }
